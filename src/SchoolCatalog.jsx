d5affb64b0fcc6920e95cf3116ed5802728c4c50
Matt Krozel <mattkrozel@Kaylis-MBP.attlocal.net>
2024-09-05 19:44:21 -0500

    push task 1, fetched data from api, rows load from api

--- src/SchoolCatalog.jsx
+++ src/SchoolCatalog.jsx
@@ -1,4 +1,12 @@
+import { useState, useEffect } from 'react';
 export default function SchoolCatalog() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/courses.json')
+    .then(response => response.json())
+    .then(data => setCourses(data));
+  }, []);
   return (
     <div className="school-catalog">
       <h1>School Catalog</h1>
@@ -8,43 +16,25 @@ export default function SchoolCatalog() {
           <tr>
             <th>Trimester</th>
             <th>Course Number</th>
-            <th>Courses Name</th>
+            <th>Course Name</th>
             <th>Semester Credits</th>
             <th>Total Clock Hours</th>
             <th>Enroll</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>PP1000</td>
-            <td>Beginning Procedural Programming</td>
-            <td>2</td>
-            <td>30</td>
-            <td>
-              <button>Enroll</button>
-            </td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>PP1100</td>
-            <td>Basic Procedural Programming</td>
-            <td>4</td>
-            <td>50</td>
-            <td>
-              <button>Enroll</button>
-            </td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>OS1000</td>
-            <td>Fundamentals of Open Source Operating Systems</td>
-            <td>2.5</td>
-            <td>37.5</td>
-            <td>
-              <button>Enroll</button>
-            </td>
-          </tr>
+          {courses.map((course) => (
+            <tr key={course.courseNumber}>
+              <td>{course.trimester}</td>
+              <td>{course.courseNumber}</td>
+              <td>{course.courseName}</td>
+              <td>{course.semesterCredits}</td>
+              <td>{course.totalClockHours}</td>
+              <td>
+                <button>Enroll</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <div className="pagination">
